dde48feff480532c1422d7d1f78ac8780bc4be23
sameer-ahmed123 <sameerkhanjan123@gmail.com>
2023-02-02 00:10:03 +0500

    table (Remove/Adding) in 'First connect => folder'

--- First connect/first connect.py
+++ First connect/first connect.py
@@ -1,18 +1,22 @@
 import sqlite3
 
 
-connect = sqlite3.connect("data.db")
+connect = sqlite3.connect("First connect/data.db")
 
 
 
 cursor = connect.cursor()
-cursor.execute("""
-     DROP table Users;
-""")
 
+#this checks if the table already exists if so then deletes data in  the table 'Users' then adds the new data 
+table_check_query = "SELECT name FROM sqlite_master WHERE type='table' AND name='Users'"
+if cursor.execute(table_check_query).fetchone():
+    cursor.execute("DELETE FROM Users")
+    print("Data in the 'Users' table has been deleted.")
+else:
+    print("The 'Users' table does not exist.")
 
 cursor.execute("""
-    CREATE table Users (
+    CREATE table IF NOT EXISTS Users (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         name TEXT,
         email TEXT,
@@ -24,7 +28,20 @@ cursor.execute("""
 
 cursor.execute("""
     INSERT INTO Users (name,email,address,AGE,Hobby) 
-    VALUES ('SAMEER','sameerkhanjan123@gmail.com','Rawalpindi','20','coding')
+    VALUES ('SAMEER','sameerkhanjan123@gmail.com','Rawalpindi','20','coding'),
+    ('saeed','saeed@gmail.com','pindi','19','sleeping'),
+    ('ali','ali123@gmial.com','lahore','14','gaming'),
+    ('sadia','sadi123@yahoo.com', 'paris','19','singing')
+
 """)
 
+a = connect.cursor()
+a.execute("""
+    SELECT * FROM Users Where age = '19'; 
+""")
+
+# print(a.fetchall())
+rows = a.fetchall()
+for row in rows:
+    print(row)
 connect.commit()
